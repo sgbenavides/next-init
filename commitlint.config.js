0bdd20520b087a32a224ee8e032e9d8a087382c7
Sergio Benavides <sergiobenav@proton.me>
2025-03-10 09:57:46 -0600

    chore: Update husky and commitlint configuration

---
+++ commitlint.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // New feature
+        'fix', // Bug fix
+        'docs', // Documentation
+        'style', // Styling
+        'refactor', // Code refactoring
+        'perf', // Performance improvements
+        'test', // Adding tests
+        'chore', // Maintenance
+        'ci', // CI/CD
+        'build', // Build system
+        'revert', // Revert changes
+      ],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'subject-case': [2, 'always', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [2, 'always'],
+  },
+};
